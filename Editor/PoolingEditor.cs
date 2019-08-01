b58fbc1094eee633781bcf27f6f2e4ffd790531b
qc-leblond <antoineqc@hotmail.com>
2019-08-01 18:07:52 -0400

    Added auto generation of the pooling settings

---
+++ Editor/PoolingEditor.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Atuvu.Pooling
+{
+    static class PoolingEditor
+    {
+        [InitializeOnLoadMethod]
+        static void EnsurePoolSettingsExist()
+        {
+            if (!PoolManagerSettings.settingsFileExists)
+            {
+                var assets = AssetDatabase.FindAssets("t:PoolManagerSettings");
+                PoolManagerSettings settings = null;
+                foreach (var assetPath in assets)
+                {
+                    settings = AssetDatabase.LoadAssetAtPath<PoolManagerSettings>(assetPath);
+                    if (settings != null)
+                        break;
+                }
+
+                if (settings == null)
+                {
+                    settings = ScriptableObject.CreateInstance<PoolManagerSettings>();
+                    AssetDatabase.CreateAsset(settings, "Assets/PoolingSettings.asset");
+
+                }
+
+                var preLoadedAssets = PlayerSettings.GetPreloadedAssets().ToList();
+                preLoadedAssets.Add(settings);
+                PlayerSettings.SetPreloadedAssets(preLoadedAssets.ToArray());
+            }
+        }
+    }
+}
---
+++ Runtime/AssemblyFriends.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Atuvu.Pooling.Editor")]
+[assembly: InternalsVisibleTo("Atuvu.Pooling.Tests")]
